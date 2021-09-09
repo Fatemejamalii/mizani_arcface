6d9a4e83b325f307c355074070cad4008259f0df
Fatemejamalii <89065802+Fatemejamalii@users.noreply.github.com>
2021-09-09 14:14:35 +0430

    Update attr_encoder.py

--- model/attr_encoder.py
+++ model/attr_encoder.py
@@ -28,4 +28,4 @@ class AttrEncoder(Model):
 
     def my_save(self, reason=''):
         self.model.save_weights(str(self.args.weights_dir.joinpath(self.__class__.__name__ + reason + '.h5')))
-        self.model.save_weights(str(self.args.weights_dir_for_test(self.__class__.__name__ + '.h5')))
+        self.model.save_weights(str(self.args.weights_dir_for_test.joinpath(self.__class__.__name__ + '.h5')))
